abcec51a806bd565365ad91fb5b5061d1a29665f
Richard Collier <ergosoftuk@gmail.com>
2017-08-23 12:53:57 +0100

    Updated to new Angular4 common

--- FrontEnd/src/app/app-menu.service.ts
+++ FrontEnd/src/app/app-menu.service.ts
@@ -4,13 +4,13 @@ import {MenuOption} from "eds-angular4/dist/layout/models/MenuOption";
 
 @Injectable()
 export class AppMenuService implements  MenuService {
+  getClientId(): string {
+    return 'eds-portal';
+  }
   getApplicationTitle(): string {
-    return 'Information Modeller';
+    return 'Discovery Launch Vehicle';
   }
   getMenuOptions():MenuOption[] {
-    return [
-      {caption: 'Concept Modeller', state: 'conceptModeller', icon: 'fa fa-sitemap'},
-      {caption: 'IM Settings', state: 'settings', icon: 'fa fa-cogs'}
-    ];
+    return [];
   }
 }
